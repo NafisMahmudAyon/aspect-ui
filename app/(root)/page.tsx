33a0093db4e5fee1451a1d195e1b63a80a89515b
NafisMahmudAyon <nafismahmudayon@gmail.com>
2025-07-07 09:04:10 +0600

    0.0.111

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,6 @@ import ExploreComponents from '@/components/ExploreComponents'
 import Hero from '@/components/Hero'
 import Footer from '@/components/Footer'
 import React from 'react'
-import SidebarDemo from '@/components/test/Sidebar'
 // import UserButton from '@/components/UserButton'
 
 const page = () => {
@@ -14,7 +13,6 @@ const page = () => {
       <ExploreComponents />
       <div className='h-[300px]' />
       {/* <UserButton />/ */}
-      <SidebarDemo />
       <Footer />
     </div>
   )
