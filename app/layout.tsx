e92786a0d46426f52ea6bf5272b0f6f704122812
NafisMahmudAyon <nafismahmudayon@gmail.com>
2025-08-11 19:59:36 +0600

    component pages updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ const montserrat = Montserrat({
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
       <body className={`text-text-muted scrollbar-thin font-sans ${montserrat.className}`} suppressHydrationWarning>
         <RouteProgress /> {/* Place early for better timing */}
         <ClerkProvider>
